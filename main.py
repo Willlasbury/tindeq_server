521a81a81c1734d05cc995ee39b07b4be80062a0
Willlasbury <asburyw229@gmail.com>
2023-10-28 18:02:19 -0700

    handled cors

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://127.0.0.1:5173"]
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+# todo: handle CORS
+@app.get("/test")
 async def root():
     return {"message": "Hellofdsa World"}

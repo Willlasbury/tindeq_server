6d4f0decdafcb21234110b4553f4fcec926198dc
Willlasbury <asburyw229@gmail.com>
2023-11-17 11:35:55 -0700

    created routes folder and moved endpoints

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from tindeq import TindeqHandler
-from schemas.req.tindeqData import TindeqData
-from schemas.res.weightRes import WeightRes
-from schemas.req.weightData import Weight
-
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-from supabase import create_client, Client
+from routes import tindeq, weights
 
 app = FastAPI()
 
@@ -24,41 +14,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-tindeq = TindeqHandler()
-
-
-
-
-@app.get("/tindeq")
-async def root():
-    return {"message": "Hello World"}
-
-# todo: add response schema
-@app.post("/tindeq")
-async def getTindeqData(data: TindeqData):
-    try:
-        res = tindeq.handleData(data.bytes)
-        return res
-    except:
-        return 'error'
-
-# route for testing supabase connection
-# ONLY WORKS WITH RLS DISABLED ON SUPABASE TABLE
-@app.post("/measurement")
-async def insert(data: Weight):
-    value = {'weight':data.weight}
-    url : str = os.getenv('SUPABASE_URL')
-    key : str = os.getenv('API_KEY')
-    supabase: Client = create_client(url, key)
-    res = supabase.table('max_weight').insert(value).execute()
-    return res
-
-
-@app.post("/max_weight")
-async def handle_max_weight(data: Weight):
-    value = {'weight':data.weight}
-    url : str = os.getenv('SUPABASE_URL')
-    key : str = os.getenv('API_KEY')
-    supabase: Client = create_client(url, key)
-    res = supabase.table('max_weight').insert(value).execute()
-    return res
+app.include_router(tindeq.router)
+app.include_router(weights.router)

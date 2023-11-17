3e90b9f25cf47a58da008089f8ca603cc45b00de
Willlasbury <asburyw229@gmail.com>
2023-11-17 12:25:31 -0700

    added max weight response model

--- routes/weights.py
+++ routes/weights.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 from supabase import create_client, Client
 
 from schemas.req.weightData import Weight
+from schemas.res.max_weight import MaxWeightRes
 
 import os
 from dotenv import load_dotenv
@@ -11,7 +12,7 @@ router = APIRouter(prefix='/weights')
 
 # route for testing supabase connection
 # ONLY WORKS WITH RLS DISABLED ON SUPABASE TABLE
-@router.post("", status_code=status.HTTP_201_CREATED)
+@router.post("", status_code=status.HTTP_201_CREATED, response_model=MaxWeightRes)
 async def handle_max_weight(data: Weight):
     value = {'weight':data.weight}
     url : str = os.getenv('SUPABASE_URL')
@@ -21,4 +22,5 @@ async def handle_max_weight(data: Weight):
     # need to check what supabase errors look like
     if not res.data[0]:
         raise HTTPException(status_code=502, detail="Data not added to database")
+    print('res.data[0]: ', res.data[0])
     return res.data[0]

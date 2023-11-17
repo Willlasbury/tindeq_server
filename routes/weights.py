9007a8ac6b4aec7d33afd52caceeb449fcdd0893
Willlasbury <asburyw229@gmail.com>
2023-11-17 12:12:17 -0700

    added more descriptive http codes to weights post

--- routes/weights.py
+++ routes/weights.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from supabase import create_client, Client
 
 from schemas.req.weightData import Weight
@@ -11,21 +11,14 @@ router = APIRouter(prefix='/weights')
 
 # route for testing supabase connection
 # ONLY WORKS WITH RLS DISABLED ON SUPABASE TABLE
-@router.post("/test")
-async def insert(data: Weight):
-    value = {'weight':data.weight}
-    url : str = os.getenv('SUPABASE_URL')
-    key : str = os.getenv('API_KEY')
-    supabase: Client = create_client(url, key)
-    res = supabase.table('max_weight').insert(value).execute()
-    return res
-
-
-@router.post("")
+@router.post("", status_code=status.HTTP_201_CREATED)
 async def handle_max_weight(data: Weight):
     value = {'weight':data.weight}
     url : str = os.getenv('SUPABASE_URL')
     key : str = os.getenv('API_KEY')
     supabase: Client = create_client(url, key)
     res = supabase.table('max_weight').insert(value).execute()
-    return res
+    # need to check what supabase errors look like
+    if not res.data[0]:
+        raise HTTPException(status_code=502, detail="Data not added to database")
+    return res.data[0]

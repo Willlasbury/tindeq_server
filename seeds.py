c992c3314de5842a7157b7fd4805878bf686ed1c
Willlasbury <asburyw229@gmail.com>
2023-11-20 16:49:14 -0700

    created delete endpoint for all rows in max weight table

--- seeds.py
+++ seeds.py
@@ -1,4 +1,5 @@
 from routes.weights import create_max_weight
+from routes.weights import delete_max_weights
 from routes.tindeq import root
 import numpy as np
 
@@ -13,7 +14,9 @@ for i in range(1,5):
     rand = i * np.random.random() * 10
     for j in range(1, 3):
         extra = rand * j
-        seeds.append(extra)
+        seeds.append(round(extra,1))
+
+asyncio.run(delete_max_weights())
 
 for weight in seeds:
     data = Weight(weight)

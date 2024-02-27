2f9eedbe94dd92fc627e30cc4da87ef1b4d786ec
Willlasbury <asburyw229@gmail.com>
2024-02-27 13:56:29 -0700

    limit style data to only certain grips and hand
    update styledata model with typing literal from grip and hand

--- schemas/req/styleData.py
+++ schemas/req/styleData.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from typing import Literal
+
 
 class StyleData(BaseModel):
     edge_size_mm: int
-    grip: str
-    hand: str
+    grip: Literal["full", "half", "open"]
+    hand: Literal["left", "right"]
     index: bool
     middle: bool
     ring: bool

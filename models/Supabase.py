15391b6be16c057e0393ef951fe7401520c06bc7
Willlasbury <asburyw229@gmail.com>
2024-03-02 14:00:26 -0700

    get user data from supabase

--- models/Supabase.py
+++ models/Supabase.py
@@ -5,41 +5,70 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-url: str = os.getenv("SUPABASE_URL")
+supabase_url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("API_KEY")
 
 
 class SupabaseReq:
     def __init__(self, table=None) -> None:
-        self.url = url
+        self.url = supabase_url
         self.key = key
-        self.endpoint = f"rest/v1/{table}" if table else "/auth/v1/token"
+        self.auth_url = "auth/v1"
+        self.rest_url = "rest/v1"
+
+        # Reference endpoints from supabase-py
+        # self.realtime_url = f"{supabase_url}/realtime/v1".replace("http", "ws")
+        # self.auth_url = f"{supabase_url}/auth/v1"
+        # self.storage_url = f"{supabase_url}/storage/v1"
+        # self.functions_url = f"{supabase_url}/functions/v1"
+        
         self.header = {"Content-Type": "application/json", "apikey": self.key}
 
-    def _param_payload(self, params):
+    def _end_point(self, table):
+        return f"rest/v1/{table}" 
+    
+    # create custom string to add as params since I am having trouble 
+    #   with special character (e.g. *) returning as %... 
+    def _param_payload(self, params): 
         return "&".join("%s=%s" % (k, v) for k, v in params.items())
 
+    # add any headers other than the default
     def _headers(self, token, **kwargs):
         if kwargs.get('custom_headers') is not None:
             return {**self.header, **kwargs.get('custom_headers'), "Authorization":f"Bearer {token}"}
         else:
             return {**self.header, "Authorization":f"Bearer {token}"}
 
-    def _url(self):
-        return f"{self.url}/{self.endpoint}"
+    # create the url path
+    def _url(self, type, endpoint):
+        return f"{self.url}/{type}/{endpoint}"
 
-    def get(self, session_token, params):
-        url = self._url()
-        header = self._headers(token=session_token)
+    def get_user_data(self, session_token):
+        # param_string = self._param_payload({'grant_type':'password'})
+        headers = self._headers(token=session_token)
+        res = requests.get(url=self._url(self.auth_url, 'user'), headers=headers)
+        print('res: ', res.request.url)
+        return res
+
+    def get(self, table, session_token, params):
+        url = self._url(self.rest_url, table)
+        print('url: ', url)
+        headers = self._headers(token=session_token)
         param_string = self._param_payload(params)
         res = requests.get(
             url=url,
             params=param_string,
-            headers=header,
+            headers=headers,
         )
         return res
 
-    def post(self, endpoint, json, headers):
-        requests.post(
-            f"{self.url}/{endpoint}", json=json, headers={"apiKey": self.key, **headers}
+    def post(self, session_token, json):
+        url = self._url()
+        headers = self._headers(token=session_token)
+        res = requests.get(
+            url=url,
+            headers=headers,
+            json=json
         )
+        return res
+
--- routes/weights.py
+++ routes/weights.py
@@ -17,12 +17,11 @@ requester = SupabaseReq(table='max_pull')
 
 router = APIRouter(prefix="/max_pull")
 
-
 # ONLY WORKS WITH RLS DISABLED ON SUPABASE TABLE
 @router.get("")
 async def get_max_pulls(request: Request):
     token = request.headers.get("Authorization")
-    res = requester.get(params={'select':'*'}, session_token=token)
+    res = requester.get(table='max_pull', params={'select':'*'}, session_token=token)
     return res.json()
 
 # removed res modal remember to put back in
@@ -40,12 +39,17 @@ async def create_max_pull(
         "ring": data.style.ring,
         "pinky": data.style.pinky,
         }
-    # print('\ntest\n')
+    
     token = request.headers.get("Authorization")
-    user_data = supabase.auth.get_user(token)
-    user_id = user_data.user.id
-    style = supabase.table('style').select('*').match(style).execute()
-    style_id = style.data[0].get('id')
+
+    # get user data
+    user_data = requester.get_user_data(token)
+    return {'data':user_data.json()}
+    # user_id = user_data.user.id
+
+    #get style id
+    # style = supabase.table('style').select('*').match(style).execute()
+    # style_id = style.data[0].get('id')
     # if unique.data == []:
     #     style_data = supabase.table('style').insert(style).execute()
     # return token, supabase._auth_token
@@ -55,17 +59,17 @@ async def create_max_pull(
         "style_id": style_id,
         "weight_kg": data.weight
     }
-    try:
+    # try:
         # supabase._auth_token = {'apiKey': key, 'Authorization': f"Bearer {token}"}
         # print('\nsupabase auth header: ', supabase._get_token_header(),"\n")
-        res = supabase.table("max_pull").insert(obj).execute()
-        # res = requests.post("https://dkmvspmsglplyfthgrxb.supabase.co/rest/v1/max_pull", json=obj, headers={'apiKey': key, 'Authorization': f"Bearer {token}"})
-        return res
-        # return {'message':"sucess"}
-    except Exception as e:
+    #     res = supabase.table("max_pull").insert(obj).execute()
+    #     # res = requests.post("https://dkmvspmsglplyfthgrxb.supabase.co/rest/v1/max_pull", json=obj, headers={'apiKey': key, 'Authorization': f"Bearer {token}"})
+    #     return res
+    #     # return {'message':"sucess"}
+    # except Exception as e:
 
-        return {'message':"error"}
-        # raise HTTPException(status_code=400, detail=e.message)
+    #     return {'message':"error"}
+    #     # raise HTTPException(status_code=400, detail=e.message)
     
 
 # Delete this end point as soon as you want to store real data
